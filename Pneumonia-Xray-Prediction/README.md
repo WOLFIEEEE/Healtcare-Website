94306a6b99a5ce41852027151cf6be8a4bd4ba4d
KHUSHWANT PARIHAR <50989808+WOLFIEEEE@users.noreply.github.com>
2021-04-28 21:54:30 +0530

    Adding Pnuemonia Deployment Model

---
+++ Pneumonia-Xray-Prediction/README.md
@@ -0,0 +1,2 @@
+# Pneumonia-Xray-Prediction
+Pneumonia-Xray-prediction model trained using X-ray imgaes . We used CNN algrtihm to extract feature from Images and train our model based on that paramater
---
+++ Pneumonia-Xray-Prediction/app/main.py
@@ -0,0 +1,83 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+import numpy as np
+from keras.preprocessing import image
+import tensorflow as tf
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+
+# Load your trained model
+try:
+    model =tf.keras.models.load_model('New_model')
+except:
+    print("model not loaded")
+# model._make_predict_function()          # Necessary
+# print('Model loaded. Start serving...')
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+#from keras.applications.resnet50 import ResNet50
+#model = ResNet50(weights='imagenet')
+#model.save('')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def model_predict(img_path, model):
+    test_image = image.load_img(img_path, target_size=(64, 64))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis = 0)
+    result = model.predict(test_image)
+
+    if result[0][0] == 1:
+        prediction = 'covid'
+    else:
+        prediction = 'normal'
+    print(prediction)
+    return prediction
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        return preds
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
